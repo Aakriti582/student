b71890c6da19dc22f73ddfed5e8b58190634b87f
Aakriti582 <aakritilimbu308@gmail.com>
2026-01-24 21:50:57 +0545

    very imp concept cleared composition and contracts/interface through ABCused today

--- abstraction.py
+++ abstraction.py
@@ -9,7 +9,7 @@ class ResultPolicy(ABC):
 
     @abstractmethod
     def is_passed(self,marks:int) ->bool:
-        return marks>=50
+        pass
         
 
 class UGResultPolicy(ResultPolicy):
@@ -31,8 +31,7 @@ class Student:
 #composition here because result  has a resultpolicy
 class Result:
     def __init__(self,marks:int,policy:ResultPolicy):
-        self.marks=marks
-        self._marks=marks
+        self.marks=marksss        self._marks=marks
         self.policy=policy#Composition happens here
 
     def is_passed(self):
@@ -49,23 +48,9 @@ class Result:
         else:
             raise InvalidMarksError(newmarks)
 
-class UGStudent(Student):
-    def __init__(self,id,name,age,course_duration,ugresult:PGResultPolicy):
-        super().__init__(id,name,age)
-        self.course_duration=course_duration
-        self.ugresult=ugresult
-    
-
-class PGStudent(Student):
-    def __init__(self,id,name,age,course_duration,pgresult:PGResultPolicy):
-        super().__init__(id,name,age)
-        self.course_duration=course_duration
-        self.pgresult=pgresult
-
-
-ugstudent=UGStudent(1,"aakriti",19,4,UGResultPolicy())
+ugstudent=Student(1,"aakriti",19)
 ugresult=Result(50, UGResultPolicy())
-pgstudent=PGStudent(2,"Khushi",18,2,PGResultPolicy())
+pgstudent=Student(2,"Khushi",18)
 pgresult=Result(40,PGResultPolicy())
 print(ugstudent.name)
 print(ugresult.is_passed())
