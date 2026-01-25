ec524f185d34a995cae4619558e891e9c169ff15
Aakriti582 <aakritilimbu308@gmail.com>
2026-01-25 20:50:07 +0545

    enforced contract and liskiov substitution principle

--- abstraction.py
+++ abstraction.py
@@ -22,17 +22,24 @@ class PGResultPolicy(ResultPolicy):
     def is_passed(self,marks):
         return marks>=50
     
+class BrokenPolicy(ResultPolicy):
+    def is_passed(self,marks):
+        return "yes"
+
 class Student:
     def __init__(self,id,name,age):
         self.id=id
         self.name=name
         self.age=age
 
+
 #composition here because result  has a resultpolicy
 class Result:
     def __init__(self,marks:int,policy:ResultPolicy):
-        self.marks=marksss        self._marks=marks
+        self.marks=marks
+        self._marks=marks
         self.policy=policy#Composition happens here
+        
 
     def is_passed(self):
         return self.policy.is_passed(self.marks)
@@ -48,13 +55,29 @@ class Result:
         else:
             raise InvalidMarksError(newmarks)
 
-ugstudent=Student(1,"aakriti",19)
-ugresult=Result(50, UGResultPolicy())
-pgstudent=Student(2,"Khushi",18)
-pgresult=Result(40,PGResultPolicy())
-print(ugstudent.name)
-print(ugresult.is_passed())
+#composition here
+def check_result(result:Result):
+    return result.is_passed()
+
+stu=Student(1,"aakriti",19)
+res=Result(50,UGResultPolicy())
+print(res.is_passed())
+print(check_result(res))
 print()
-print(pgstudent.name)
-print(pgresult.is_passed())
+stu1=Student(2,"khushi",10)
+res1=Result(40,PGResultPolicy())
+print(res1.is_passed())
+print(check_result(res1))
+print()
+brstu=Student(3,"richha",11)
+res2=Result(30,BrokenPolicy())
+print(res2.is_passed())
+print(check_result(res2))
+
+
+
+        
+
+
+
 
